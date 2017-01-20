05f8eed847af6fb21dff3886fa8e04e84b2ef46f
Neil Armstrong <narmstrong@baylibre.com>
2017-01-20 11:23:58 +0100

    mali_memory: Add fixes for 4.6+ kernels
    Took from https://github.com/mripard/sunxi-mali, commits :
    - d928f3dfd946 ("Add a proper fix for the 4.6+ kernels")
    - f1df8ce2b8ca ("Allow to build on a 4.8 kernel")
    
    Thanks Maxime !

--- driver/src/devicedrv/mali/linux/mali_memory.c
+++ driver/src/devicedrv/mali/linux/mali_memory.c
@@ -64,7 +64,11 @@ static int mali_mem_vma_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 	int ret;
 	int prefetch_num = MALI_VM_NUM_FAULT_PREFETCH;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+	unsigned long address = (unsigned long)vmf->address;
+#else
 	unsigned long address = (unsigned long)vmf->virtual_address;
+#endif
 	MALI_DEBUG_ASSERT(alloc->backend_handle);
 	MALI_DEBUG_ASSERT((unsigned long)alloc->cpu_mapping.addr <= address);
 
