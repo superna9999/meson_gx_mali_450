9c9bff9fbf8945b0e9520915fe435dcdf247ea2e
Joseph Kogut <joseph.kogut@gmail.com>
2017-06-18 11:11:16 -0700

    mali: memory: pass only vmf in vm operations

--- driver/src/devicedrv/mali/linux/mali_memory.c
+++ driver/src/devicedrv/mali/linux/mali_memory.c
@@ -57,9 +57,9 @@ static void mali_mem_vma_close(struct vm_area_struct *vma)
 	vma->vm_private_data = NULL;
 }
 
-static int mali_mem_vma_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
+static int mali_mem_vma_fault(struct vm_fault *vmf)
 {
-	mali_mem_allocation *alloc = (mali_mem_allocation *)vma->vm_private_data;
+	mali_mem_allocation *alloc = (mali_mem_allocation *)vmf->vma->vm_private_data;
 	mali_mem_backend *mem_bkend = NULL;
 	int ret;
 	int prefetch_num = MALI_VM_NUM_FAULT_PREFETCH;
@@ -89,7 +89,7 @@ static int mali_mem_vma_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 		MALI_DEBUG_PRINT(4, ("mali_vma_fault: do cow allocate on demand!, address=0x%x\n", address));
 		mutex_lock(&mem_bkend->mutex);
 		ret = mali_mem_cow_allocate_on_demand(mem_bkend,
-						      (address - vma->vm_start) / PAGE_SIZE);
+						      (address - vmf->vma->vm_start) / PAGE_SIZE);
 		mutex_unlock(&mem_bkend->mutex);
 
 		if (ret != _MALI_OSK_ERR_OK) {
@@ -101,7 +101,7 @@ static int mali_mem_vma_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 		 we zap the mapping in cow_modify_range, it will trigger page fault
 		 when CPU access it, so here we map it to CPU*/
 		mutex_lock(&mem_bkend->mutex);
-		ret = mali_mem_cow_cpu_map_pages_locked(mem_bkend, vma, address, prefetch_num);
+		ret = mali_mem_cow_cpu_map_pages_locked(mem_bkend, vmf->vma, address, prefetch_num);
 		mutex_unlock(&mem_bkend->mutex);
 
 		if (unlikely(ret != _MALI_OSK_ERR_OK)) {
@@ -109,7 +109,7 @@ static int mali_mem_vma_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 		}
 	} else if ((mem_bkend->type == MALI_MEM_SWAP) ||
 		   (mem_bkend->type == MALI_MEM_COW && (mem_bkend->flags & MALI_MEM_BACKEND_FLAG_SWAP_COWED))) {
-		u32 offset_in_bkend = (address - vma->vm_start) / PAGE_SIZE;
+		u32 offset_in_bkend = (address - vmf->vma->vm_start) / PAGE_SIZE;
 		int ret = _MALI_OSK_ERR_OK;
 
 		mutex_lock(&mem_bkend->mutex);
